bb7400d7bc971a893669be601f4e2fa2e03b3ebf
Deepak Naresh <deepakpamban@gmail.com>
2023-11-28 23:51:53 +0530

    Deleted Simple.java

--- Simple.java
+++
@@ -1,7 +0,0 @@
-//Your First Program
-
-class Simple{
-	public static void main(String[] args) {
-		System.out.println("Welcome to Git's Online Training!,For more infor contact me @ edurekha.co");
-	}
-}
