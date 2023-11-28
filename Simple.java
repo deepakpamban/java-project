c3de2ff80667b5e7796afe3760bf1843ff715903
Deepak Naresh <deepakpamban@gmail.com>
2023-11-29 00:11:27 +0530

    Revert "Deleted Simple.java"
    This reverts commit bb7400d7bc971a893669be601f4e2fa2e03b3ebf.

---
+++ Simple.java
@@ -0,0 +1,7 @@
+//Your First Program
+
+class Simple{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git's Online Training!,For more infor contact me @ edurekha.co");
+	}
+}
